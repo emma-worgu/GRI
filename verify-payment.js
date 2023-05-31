6eaee459019cdee7080145b5cbc5cde6d529f53f
Emmanuel Worgu <emmanuelworgu@gmail.com>
2023-05-31 01:56:05 +0100

    fixed payment

--- verify-payment.js
+++ verify-payment.js
@@ -1,21 +1,32 @@
 async function verifyPayment (payment) {
     console.log(payment);
 
+    console.log(payment.transaction_id);
+
     try {
-        const url = 'https://jwhite.onrender.com/api/user/verify-payment';
+        const url = 'http://localhost:5000/api/user/verify-payment';
+
+
         const data = {
-            transaction_id: payment.id,
+            transaction_id: payment.transaction_id,
         };
+
+        console.log(JSON.stringify(data));
 
         const req = await fetch(url, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
                 'auth-token': localStorage.getItem('token'),
+                'tranx-id': payment.transaction_id,
+                // 'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Mode': 'no-cors',
             },
         });
 
         const res = await req.json();
+        console.log(req.body);
         console.log(res);
     } catch (error) {
         alert('Error confirming your payment. Try Again!');
